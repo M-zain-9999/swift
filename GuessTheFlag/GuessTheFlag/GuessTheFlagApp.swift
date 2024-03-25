6001993f7b08d49b4f4e253fd3ccf143ac0bdf9b
M-zain-9999 <141211040+M-zain-9999@users.noreply.github.com>
2024-03-25 19:18:05 +0500

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Zain Ilyas on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
